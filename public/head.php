eb6856ab02d105b0b9345f2a50336df3ddd6c9c7
tomas <tomas@debian.tomas>
2026-07-26 15:55:38 -0400

    primer commit

---
+++ public/head.php
@@ -0,0 +1,16 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Di Matteo odontologia</title>
+
+  <meta name="description" content="Venta de sillones odontologícos y equipamiento profesional.">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="./assets/css/reset.css">
+  <link rel="stylesheet" href="./assets/css/global.css">
+  <link rel="stylesheet" href="./assets/css/components.css">
+
+</head>
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php include "./head.php"; ?>
+
+<body class="container">
+
+  <?php include "../app/Views/header.php"; ?>
+
+
+</body>
+
+
+</html>
