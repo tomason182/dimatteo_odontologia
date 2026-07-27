00e6af287d30de69b3892e0d5eb36688fee08c4b
tomas <tomas@debian.tomas>
2026-07-27 11:00:52 -0300

    add: se agrego img y bg al hero

--- app/Views/hero.php
+++ app/Views/hero.php
@@ -1,6 +1,6 @@
 <section class="hero">
   <div class="hero_container">
-    <img src="/assets/img/hero/sillon_unplush.jpg" alt="sillón odontológico">
+    <img src="/assets/img/hero/sillon_gpt.png">
   </div>
   <div class="hero_container">
     <span class="hero_eyebrow">Equipamiento odontológico para profesionales.</span>
@@ -9,6 +9,11 @@
     <p class="hero_description">Distribuimos sillones odontológicos de ls principales marcas,
       brindamos asesoramiento personalizado, instalación profesional y servicio técnico oficial.
     </p>
+
+    <div class="hero_actions">
+      <a class="hero_btn hero_btn_primary"> Consultar por whatapp</a>
+      <a class="hero_btn hero_btn_secondary">Ver modelos</a>
+    </div>
 
 
   </div>
