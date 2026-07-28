d9cc852af662d19c78ffd3ee5f411cb881c7f3da
tomas <tomas@debian.tomas>
2026-07-28 16:21:21 -0400

    add: se creo la entidad Chair

--- app/Entities/Chair.php
+++ app/Entities/Chair.php
@@ -7,6 +7,9 @@ class Chair
     public readonly string $brand,
     public readonly string $name,
     public readonly string $description,
+    public readonly string $shortDescription,
+    public readonly array $gallery,
+    public readonly string $catalogPdg,
 
     /** @var array<string, array<string | array> */
     public readonly array $features,
