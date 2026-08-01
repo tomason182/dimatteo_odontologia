4ed344a51b640439b40987b55a7a8fee45b1c214
tomas <tomas@debian.tomas>
2026-08-01 19:38:48 -0300

    add: se instalo vlucas dotenv

--- public/index.php
+++ public/index.php
@@ -3,15 +3,23 @@
 declare(strict_types=1);
 require_once __DIR__ . "/../vendor/autoload.php";
 
-ini_set("display_errors", 1);
-ini_set("display_startu_errors", 1);
-error_reporting(E_ALL);
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeload();
+
+var_dump($_ENV["APP_ENV"]);
+
+if ($_ENV["APP_ENV"] === "production") {
+  ini_set("display_errors", "0");
+  ini_set("log_errors", "1");
+  error_reporting(E_ALL);
+} else {
+  ini_set("display_errors", 1);
+  error_reporting(E_ALL);
+}
 
 require_once __DIR__ . "/../app/helpers.php";
 
-
 $page = $_GET["page"] ?? "home";
-
 
 
 switch ($page) {
